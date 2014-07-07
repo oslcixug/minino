a1bcbd9cfd24fdb9225781b4b0c8ff94586c111a
Rafael R. Gaioso <osl@cixug.es>
2014-07-07 13:28:20 +0200

    Engadimos comentarios. Eliminamos liñas obsoletas. Comprobamos a execución como root

--- rcmdr/R-install.sh
+++ rcmdr/R-install.sh
@@ -1,12 +1,23 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+   echo "Este script debe ser executado por root !!" 2>&1
+   exit 1
+fi
+
 USUARIO="minino"
 
-# echo "deb http://ftp.cixug.es/CRAN/bin/linux/debian wheezy-cran3/" >> /etc/apt/sources.list
+# Engadimos o repositorio CRAN da OSL
 
 [[ ! -e /etc/apt/sources.list.d/cran.list ]] && {
 	cat <<-EOF > /etc/apt/sources.list.d/cran.list
 	deb http://ftp.cixug.es/CRAN/bin/linux/debian wheezy-cran3/
 	EOF
 }
+
+# Descargamos e instalamos a chave para os servidores de CRAN
+# En caso de non poder executarse debido a unha devasa, podemos
+# descargalo desde ftp://ftp.cixug.es/pub/cran.gpg
 
 apt-key adv --keyserver keys.gnupg.net --recv-key 381BA480
 
@@ -20,24 +31,20 @@ R CMD INSTALL /tmp/tcltk2.tar.gz
 wget http://ftp.cixug.es/CRAN/src/contrib/Rcmdr_2.0-4.tar.gz -O /tmp/rcmdr.tar.gz
 R CMD INSTALL /tmp/rcmdr.tar.gz
 
-## Instalación dependencias do paquete «r»
-## dpkg -I nome_paquete_R.deb > depends.txt
-
-LISTA="paquete1 paquete2 paquete3 ... paqueteN"
-apt-get install -y --force-yes $LISTA
-
-## Instaladas as dependencias, instalamos o paquete principal
-dpkg -i paquete.deb
-
+# Engadimos o script a executar no inicio de R
+# na configuración do USUARIO como no esqueleto de novos usuarios
 
 wget -O- -q ftp://ftp.cixug.es/pub/Rprofile > /tmp/Rprofile
 cp /tmp/Rprofile  /home/$USUARIO/.Rprofile
 cp /tmp/Rprofile  /etc/skel/.Rprofile
 
-#wget -O- -q ftp://ftp.cixug.es/pub/left > /home/$USUARIO/.config/lxpanel/LXDE/panels/left
+# Descargamos o ficheiro coa nova configuración do panel esquerdo coa icona para abrir R
+# copiamos o ficheiro dentro da configuración do USUARIO e do esqueleto para novos usuarios
+
 wget -O- -q ftp://ftp.cixug.es/pub/left > /tmp/left
 
 cp /tmp/left /home/$USUARIO/.config/lxpanel/LXDE/panels/left
 cp /tmp/left /home/$USUARIO/.config/lxpanel/GNOME2/panels/left
+
 cp /tmp/left /etc/skel/.config/lxpanel/LXDE/panels/left
 cp /tmp/left /etc/skel/.config/lxpanel/GNOME2/panels/left
